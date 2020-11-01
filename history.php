e4cbaa3789e4ef88cec4adf275cbbbd5fd689ff5
PuPeepPuPeep <cjsonastv@gmail.com>
2020-11-01 20:55:03 +0700

    Add payment AND history Sys

---
+++ history.php
@@ -0,0 +1,16 @@
+<?php
+include('connect.php');
+session_start();
+
+$insertsql = "INSERT INTO history(M_ID,O_ID) SELECT M_ID, O_ID FROM orders WHERE O_status=1 AND O_ID NOT IN (
+    SELECT O_ID FROM history)";
+$excute = mysqli_query($conn, $insertsql);
+if($excute){
+    echo '<script>
+        alert("ฮิคฮิ้ววววว");
+        window.location.href = "index.php";
+    </script>';
+}else{
+    echo 'รวยเถอะ'.mysqli_error($conn);
+}
+?>
--- payment.php
+++ payment.php
@@ -2,11 +2,16 @@
 session_start();
 include('connect.php');
 
-$sql = "SELECT * FROM orders WHERE M_ID ='".$_SESSION['M_ID']."'";
-$result = mysqli_query($conn, $sql);
-echo $_SESSION['M_ID'];
-while($row = mysqli_fetch_assoc($result)){
-    echo $row['O_ID'];
+$sum = $_GET['sum'];
+
+$updatesql = "UPDATE orders SET O_status=1 WHERE M_ID ='".$_SESSION['M_ID']."' AND O_status=0";
+$excute = mysqli_query($conn, $updatesql);
+$insertsql = "INSERT INTO payment(Pay_price,M_ID) VALUES('".$sum."','".$_SESSION['M_ID']."')";
+$result = mysqli_query($conn, $insertsql);
+if($excute and $result){
+    echo 'ชำระเงินสำเร็จ';
+    header("Location:history.php");
+}else{
+    echo 'พัง';
 }
-echo $_GET['sum'];
 ?>
