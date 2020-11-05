027f63c174b882e0330783327ad6bbbde99cb80c
PuPeepPuPeep <cjsonastv@gmail.com>
2020-11-05 22:19:55 +0700

    Fix Map

--- footer.php
+++ footer.php
@@ -16,8 +16,8 @@
           <button type="submit" class="w3-button w3-block w3-dark-gray">Send</button>
         </form>
       </div>
-      <div class="w3-col s4">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3854.089665686096!2d102.11553831484484!3d14.987726989556792!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31194b80019e1771%3A0xc6979e02d8d878c7!2z4Lih4Lir4Liy4Lin4Li04LiX4Lii4Liy4Lil4Lix4Lii4LmA4LiX4LiE4LmC4LiZ4LmC4Lil4Lii4Li14Lij4Liy4LiK4Lih4LiH4LiE4Lil4Lit4Li14Liq4Liy4LiZ!5e0!3m2!1sth!2sth!4v1604242070500!5m2!1sth!2sth" width="700" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+      <div class="w3-col s8">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3854.089665686096!2d102.11553831484484!3d14.987726989556792!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31194b80019e1771%3A0xc6979e02d8d878c7!2z4Lih4Lir4Liy4Lin4Li04LiX4Lii4Liy4Lil4Lix4Lii4LmA4LiX4LiE4LmC4LiZ4LmC4Lil4Lii4Li14Lij4Liy4LiK4Lih4LiH4LiE4Lil4Lit4Li14Liq4Liy4LiZ!5e0!3m2!1sth!2sth!4v1604242070500!5m2!1sth!2sth" width="100%" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
       </div>
 
       
