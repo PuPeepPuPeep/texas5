dd2634f3cdb80f94348e3b52eef3062307fedc14
PuPeepPuPeep <cjsonastv@gmail.com>
2020-11-06 21:57:37 +0700

    Insert Product To Database

--- texas5.sql
+++ texas5.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 01, 2020 at 02:52 PM
+-- Generation Time: Nov 06, 2020 at 03:55 PM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.4.8
 
@@ -47,10 +47,19 @@ INSERT INTO `admin` (`A_ID`, `A_Password`) VALUES
 
 CREATE TABLE `feedback` (
   `F_ID` int(11) NOT NULL,
-  `F_Date` date NOT NULL,
+  `F_Date` date DEFAULT current_timestamp(),
   `F_Text` varchar(200) NOT NULL,
   `M_ID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `feedback`
+--
+
+INSERT INTO `feedback` (`F_ID`, `F_Date`, `F_Text`, `M_ID`) VALUES
+(1, '2020-11-04', 'Test', 1),
+(2, '2020-11-04', 'บ่มีอิหยังมาพังทล๊ายยยยยยย', 1),
+(3, '2020-11-05', 'สวัส ดีคร้าบบ', 4);
 
 -- --------------------------------------------------------
 
@@ -70,9 +79,6 @@ CREATE TABLE `history` (
 --
 
 INSERT INTO `history` (`H_ID`, `H_Date`, `M_ID`, `O_ID`) VALUES
-(1, '0000-00-00 00:00:00', 33, 33),
-(2, '0000-00-00 00:00:00', 55, 55),
-(3, '2020-11-01 11:34:50', 78, 87),
 (4, '2020-11-01 12:53:02', 1, 5),
 (5, '2020-11-01 12:53:02', 1, 6),
 (6, '2020-11-01 12:53:02', 1, 7),
@@ -92,7 +98,24 @@ INSERT INTO `history` (`H_ID`, `H_Date`, `M_ID`, `O_ID`) VALUES
 (26, '2020-11-01 12:53:13', 1, 12),
 (27, '2020-11-01 12:53:13', 1, 13),
 (34, '2020-11-01 13:48:18', 1, 14),
-(35, '2020-11-01 13:48:18', 1, 15);
+(35, '2020-11-01 13:48:18', 1, 15),
+(37, '2020-11-01 13:56:18', 1, 16),
+(38, '2020-11-01 13:56:18', 1, 17),
+(39, '2020-11-04 08:47:33', 1, 18),
+(40, '2020-11-04 08:47:33', 1, 19),
+(42, '2020-11-04 08:50:12', 1, 20),
+(43, '2020-11-04 08:52:55', 1, 21),
+(44, '2020-11-04 08:52:55', 1, 22),
+(46, '2020-11-04 08:54:40', 1, 23),
+(47, '2020-11-05 08:37:10', 1, 24),
+(48, '2020-11-05 08:38:08', 1, 25),
+(49, '2020-11-05 08:38:08', 1, 26),
+(50, '2020-11-05 08:38:08', 1, 27),
+(51, '2020-11-05 08:39:15', 1, 28),
+(52, '2020-11-05 08:39:15', 1, 29),
+(53, '2020-11-05 08:39:15', 1, 30),
+(54, '2020-11-05 14:17:45', 4, 31),
+(55, '2020-11-05 14:17:45', 4, 32);
 
 -- --------------------------------------------------------
 
@@ -115,7 +138,8 @@ CREATE TABLE `member` (
 INSERT INTO `member` (`M_ID`, `M_Username`, `M_Password`, `M_Name`, `M_Address`) VALUES
 (1, 'user', '1111', 'user', '1234'),
 (2, '', '', '<br /><b>Notice</b>:  Undefined index: P_ID in <b>C:xampphtdocs	exas5edit.php</b> on line <b>14</b><', ''),
-(3, 'user5', '5555', 'user5', 'abcd');
+(3, 'user5', '5555', 'user5', 'abcd'),
+(4, 'mhee', '1111', 'หมีเหงา', 'บ้าน');
 
 -- --------------------------------------------------------
 
@@ -149,7 +173,24 @@ INSERT INTO `orders` (`O_ID`, `P_ID`, `M_ID`, `O_status`) VALUES
 (12, 16, 1, 1),
 (13, 2, 1, 1),
 (14, 11, 1, 1),
-(15, 15, 1, 1);
+(15, 15, 1, 1),
+(16, 3, 1, 1),
+(17, 3, 1, 1),
+(18, 3, 1, 1),
+(19, 2, 1, 1),
+(20, 3, 1, 1),
+(21, 2, 1, 1),
+(22, 3, 1, 1),
+(23, 3, 1, 1),
+(24, 2, 1, 1),
+(25, 2, 1, 1),
+(26, 3, 1, 1),
+(27, 16, 1, 1),
+(28, 3, 1, 1),
+(29, 2, 1, 1),
+(30, 4, 1, 1),
+(31, 3, 4, 1),
+(32, 2, 4, 1);
 
 -- --------------------------------------------------------
 
@@ -172,7 +213,17 @@ INSERT INTO `payment` (`Pay_ID`, `Pay_price`, `M_ID`) VALUES
 (2, 1666, 1),
 (3, 9645, 1),
 (4, 555, 1),
-(5, 2110, 1);
+(5, 2110, 1),
+(6, 11356, 1),
+(7, 0, 1),
+(8, 6233, 1),
+(9, 5678, 1),
+(10, 6233, 1),
+(11, 5678, 1),
+(12, 555, 1),
+(13, 15323, 1),
+(14, 7467, 1),
+(15, 6233, 4);
 
 -- --------------------------------------------------------
 
@@ -195,20 +246,50 @@ CREATE TABLE `product` (
 
 INSERT INTO `product` (`P_ID`, `P_name`, `P_price`, `P_img`, `A_ID`, `T_ID`) VALUES
 (2, 'PNOOY', 555, 'PNOOY.jpg', 'admin', 'r1'),
-(3, 'PBEWWWW', 5678, 'PBEw.jpg', 'admin', 'r11'),
-(4, 'Test', 1234, '', '', 'ram'),
-(5, 'asd', 333, '', '', 'ram'),
-(6, 'Noooo', 123444, 'C:xampp	mpphp878.tmp', '', 'ram'),
-(7, 'gkffgkgfl', 34535, 'C:xampp	mpphp4987.tmp', '', 'ram'),
-(8, 'testttttt', 5555, '', '', 'ram'),
-(9, 'AHHHHHHHH', 6666, 'Array', '', 'ram'),
-(10, 'HELPPPPP', 6666, '', '', 'ram'),
+(3, 'PBEWWWW', 5678, 'PBEw.jpg', 'admin', '3'),
+(4, 'Test', 1234, 'PBEW.jpg', '', '2'),
 (11, 'HELP ME PLZ', 1111, 'PSUN.jpg', '', 'ram'),
 (12, 'Shit here we go again', 333, 'O.jpg', '', 'ram'),
-(13, 'Ewwwwwww', 8888, 'PSUN2.jpg', '', 'ram'),
 (14, 'O No', 1234, 'O.jpg', '', 'ram'),
 (15, 'OOO', 999, 'O.jpg', '', 'ram'),
-(16, 'One More Time', 9090, 'PSUN.jpg', 'admin', 'ram');
+(16, 'One More Time', 9090, 'PSUN.jpg', 'admin', 'ram'),
+(17, 'Aha', 10, 'O.jpg', 'admin', 'ram'),
+(18, 'sss', 444, 'PSUN.jpg', 'admin', '1'),
+(19, 'CPU (ซีพียู) AMD AM4 RYZEN 5 5600X 3.7 GHz', 10590, 'cpu1.jpg', 'admin', '1'),
+(20, 'CPU (ซีพียู) INTEL 1200 CORE I9-10900KA (MARVEL AVENGER) 3.7 GHz', 18500, 'CPU2.jpg', 'admin', '1'),
+(21, 'CPU (ซีพียู) INTEL 1200 CORE I3-10100F 3.60 GHz', 2890, 'cpu3.jpg', 'admin', '1'),
+(22, 'CPU (ซีพียู) INTEL 1200 CORE I5-10600K 4.1 GHz', 9450, 'CPU4.jpg', 'admin', '1'),
+(23, 'CPU (ซีพียู) AMD AM4 RYZEN7 3800XT 3.9 GHz', 12600, 'CPU5.jpg', 'admin', '1'),
+(24, 'CPU (ซีพียู) AMD AM4 RYZEN9 3950X 3.5 GHz', 23500, 'CPU6.jpg', 'admin', '1'),
+(25, 'MAINBOARD (เมนบอร์ด) 1200 GALAX H410M', 1999, 'm1.png', 'admin', '2'),
+(26, 'MAINBOARD (เมนบอร์ด) AM4 MSI MPG B550 GAMING CARBON WIFI', 7290, 'm2.jpg', 'admin', '2'),
+(27, 'MAINBOARD (เมนบอร์ด) AM4 MSI MPG B550 GAMING CARBON WIFI', 5590, 'm4.jpg', 'admin', '2'),
+(28, 'MAINBOARD (เมนบอร์ด) AM4 MSI MPG B550 GAMING EDGE WIFI', 8990, 'm5.jpg', 'admin', '2'),
+(29, 'MAINBAORD (เมนบอร์ด) AM4 ASROCK B550 TAICHI', 9900, 'm6.jpg', 'admin', '2'),
+(30, 'MAINBAORD (เมนบอร์ด) AM4 ASROCK B550 TAICHI', 4050, 'm7.jpg', 'admin', '2'),
+(31, '64GB (32GBx2) DDR4/3200 RAM PC (แรมพีซี) THERMALTAKE TOUGHRAM RGB (WHITE) (R022R432GX2-3200C16A)', 13200, 'r1.jpg', 'admin', '3'),
+(32, '128GB (32GBx4) DDR4/3200 RAM PC (แรมพีซี) G.SKILL RIPJAWS V (F4-3200C16Q-128GVK)', 19900, 'r2.jpg', 'admin', '3'),
+(33, '64GB (32GBx2) DDR4/3200 RAM PC (แรมพีซี) G.SKILL TRIDENT Z RGB (F4-3200C16D-64GTZR)', 11000, 'r3.jpg', 'admin', '3'),
+(34, '16GB (8GBx2) DDR4/3200 RAM PC (แรมพีซี) CORSAIR DOMINATOR PLATINUM RGB (WHITE) (CMT16GX4M2C3200C16W)', 5220, 'r4.jpg', 'admin', '3'),
+(35, '16GB (8GBx2) DDR4/3600 RAM PC (แรมพีซี) G.SKILL TRIDENT Z NEO (F4-3600C18D-16GTZN)', 3890, 'r5.jpg', 'admin', '3'),
+(36, '32GB (16GBx2) DDR4/2666 RAM PC (แรมพีซี) KINGSTON HyperX FURY BLACK (HX426C16FB4K2/32)', 4290, 'r6.jpg', 'admin', '3'),
+(37, '64GB (16GBx4) DDR4/3600 RAM PC (แรมพีซี) G.SKILL TRIDENT Z NEO (F4-3600C18Q-64GTZN)', 13500, 'r7.jpg', 'admin', '3'),
+(38, '16GB (8GBx2) DDR4/3600 RAM PC (แรมพีซี) CORSAIR VENGEANCE PRO RGB (CMW16GX4M2D3600C18) (BLACK)', 4750, 'r8.jpg', 'admin', '3'),
+(39, '4 TB SSD (เอสเอสดี) CORSAIR MP400 PCIe/NVMe M.2 2280 (CSSD-F4000GBMP400) (#0432487193)', 24000, 's1.jpg', 'admin', '4'),
+(41, '1 TB SSD (เอสเอสดี) ADDLINK S68 PCIe/NVMe M.2 2280 (AD1TBS68M2P)', 4200, 's3.jpg', 'admin', '4'),
+(42, '500 GB SSD (เอสเอสดี) KINGSTON KC2000 PCIe/NVMe M.2 2280 (SKC2000M8/500G)', 4290, 's4.jpg', 'admin', '4'),
+(44, '500 GB SSD (เอสเอสดี) KINGSTON A2000 PCIe/NVMe M.2 2280 (SA2000M8/500G)', 4290, 's5.jpg', 'admin', '4'),
+(46, '500 GB SSD (เอสเอสดี) KINGSTON A2000 PCIe/NVMe M.2 2280 (SA2000M8/500G)', 4290, 's6.jpg', 'admin', '4'),
+(47, '480 GB SSD NVMe M.2 2280 (SA2000M8/500G)', 1980, 's2.jpg', 'admin', '4'),
+(48, '256 GB SSD (เอสเอสดี) GIGABYTE AORUS RGB M.2 PCIe/NVMe M.2 2280', 2290, 's7.jpg', 'admin', '4'),
+(52, 'WD BLUE 4TB', 4500, 's8.jpg', 'admin', '4'),
+(53, 'POWER SUPPLY (อุปกรณ์จ่ายไฟ) COUGAR GEX 850 - 850W (80+ GOLD)', 3790, 'p1.jpg', 'admin', '5'),
+(54, 'POWER SUPPLY (อุปกรณ์จ่ายไฟ) COOLER MASTER V850 SFX GOLD - 850W (80+ GOLD)', 4590, 'p2.jpg', 'admin', '5'),
+(55, 'POWER SUPPLY (อุปกรณ์จ่ายไฟ) CORSAIR 750W CX750F RGB (80+ BRONZE) (CP-9020227-NA)', 4190, 'p3.jpg', 'admin', '5'),
+(56, 'POWER SUPPLY (อุปกรณ์จ่ายไฟ) SILVERSTONE 750W STRIDER (80+ GOLD) (SST-ST75F-GS) V3.0', 3290, 'p4.jpg', 'admin', '5'),
+(57, 'POWER SUPPLY (อุปกรณ์จ่ายไฟ) SILVERSTONE 700W SFX (80+ GOLD) (SST-SX700-G)', 4590, 'p5.jpg', 'admin', '5'),
+(58, '  POWER SUPPLY (อุปกรณ์จ่ายไฟ) THERMALTAKE 750W SMART BM2 (80+ BRONZE)  Buy now POWER SUPPLY', 2890, 'p6.jpg', 'admin', '5'),
+(59, 'POWER SUPPLY (อุปกรณ์จ่ายไฟ) COOLER MASTER MWE 550W (80+ BRONZE)', 1790, 'p7.jpg', 'admin', '5');
 
 -- --------------------------------------------------------
 
@@ -217,9 +298,20 @@ INSERT INTO `product` (`P_ID`, `P_name`, `P_price`, `P_img`, `A_ID`, `T_ID`) VAL
 --
 
 CREATE TABLE `type` (
-  `T_ID` varchar(5) NOT NULL,
+  `T_ID` int(11) NOT NULL,
   `T_name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `type`
+--
+
+INSERT INTO `type` (`T_ID`, `T_name`) VALUES
+(1, 'cpu'),
+(2, 'board'),
+(3, 'ram'),
+(4, 'hdd'),
+(5, 'power');
 
 --
 -- Indexes for dumped tables
@@ -281,37 +373,43 @@ ALTER TABLE `type`
 -- AUTO_INCREMENT for table `feedback`
 --
 ALTER TABLE `feedback`
-  MODIFY `F_ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `F_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `history`
 --
 ALTER TABLE `history`
-  MODIFY `H_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `H_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
 
 --
 -- AUTO_INCREMENT for table `member`
 --
 ALTER TABLE `member`
-  MODIFY `M_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `M_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `O_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `O_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
 -- AUTO_INCREMENT for table `payment`
 --
 ALTER TABLE `payment`
-  MODIFY `Pay_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `Pay_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `P_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `P_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=60;
+
+--
+-- AUTO_INCREMENT for table `type`
+--
+ALTER TABLE `type`
+  MODIFY `T_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
