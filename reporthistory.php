27ef42ce8a678e404d0309cc6fb3bc8af25fccdc
PuPeepPuPeep <cjsonastv@gmail.com>
2020-11-07 16:32:37 +0700

    Improve Report

--- reporthistory.php
+++ reporthistory.php
@@ -28,12 +28,18 @@ $mpdf = new \Mpdf\Mpdf([
 $sql = "SELECT h.H_ID, h.H_Date, h.O_ID, m.M_Name FROM history AS h, member AS m WHERE h.M_ID = m.M_ID";
 $result = mysqli_query($conn, $sql);
 
+    $content .= "<h1>History Report</h1>";
+    $content .= "<table style=text-align:center;><tr><th>History ID</th>
+        <th>History Date</th>
+        <th>Member Name</th>
+        <th>Order ID</th></tr>";
 while($row=mysqli_fetch_assoc($result)){
-    $content .= "H_ID = ".$row["H_ID"];
-    $content .= "H_Date = ".$row["H_Date"];
-    $content .= "M_Name = ".$row["M_Name"];
-    $content .= "O_ID = ".$row["O_ID"]."<br>";
+    $content .= "<tr><td>".$row["H_ID"]."</td>";
+    $content .= "<td>".$row["H_Date"]."</td>";
+    $content .= "<td>".$row["M_Name"]."</td>";
+    $content .= "<td>".$row["O_ID"]."</td></tr>";
 }
+    $content .= "</table>";
 
 $mpdf->WriteHTML($content);
 $mpdf->Output();
